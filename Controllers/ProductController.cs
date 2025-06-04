474fc6f587ee73cf91bf8da7d53f615e8cdf9082
shahd.marouf <shahdjalal2018@gmail.com>
2025-06-04 04:56:00 +0300

    WIP: local changes before rebase

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        ApplicationDbContext context = new ApplicationDbContext();
+      
+       
+        public ViewResult Index()
+        {
+            var products = context.Products.ToList(); 
+            return View(products); 
+        }
+
+        public ViewResult Details( int id)
+        {
+            Console.WriteLine($"product {id}");
+            var products = context.Products.Find(id); 
+            return View(products);
+        }
+
+        public ViewResult Create()
+        {
+           
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+       
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=.;Database=MVC_11;TrustServerCertificate=True;Trusted_Connection=True");
+        }
+    }
+}
